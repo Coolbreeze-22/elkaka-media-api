945d258af07c89aeee6cf5ef44cc583d341436c9
Coolbreeze <agbokaka8@gmail.com>
2024-10-09 08:02:13 -0700

    second commit

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use('/users', userRouter);
 app.use('/migration', migrationRouter);
 
 const PORT = process.env.PORT || 5000;
+// const PORT = 5000;
 
 mongoose.connect(process.env.CONNECTION_URL)
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
